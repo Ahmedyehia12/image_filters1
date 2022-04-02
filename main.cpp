73562d5b6a260ed0e29d77a9b3aa194c2cc388c5
Ahmedyehia12 <yehiaahmed900@gmail.com>
2022-04-02 23:05:16 +0200

    updated project

--- main.cpp
+++ main.cpp
@@ -129,13 +129,9 @@ void flip() {
     }
 }
 void blurImage(){
+    readGSBMP(blur.bmp,image);
 
-    for(int i = 0;i<SIZE;i++) {
-        for (int j = 0; j < SIZE; j++) {
-            image[i][j] = (image[i - 1][j - 1] + image[i][j - 1] + image[i + 1][j - 1] + image[i - 1][j] + image[i][j] +
-                           image[i + 1][j] + image[i - 1][j + 1] + image[i][j + 1] + image[i + 1][j + 1]) / 9;
-        }
-    }
+
 
 
 
