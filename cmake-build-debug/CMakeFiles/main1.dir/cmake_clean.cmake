c751c71626e98ff34069cd9bac98ad08bfa47100
Ahmedyehia12 <yehiaahmed900@gmail.com>
2022-04-17 18:22:30 +0200

    updated project

---
+++ cmake-build-debug/CMakeFiles/main1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main1.dir/CS112-203-2nd-S3-20210049-20210067-20211053-A3-Part1.cpp.obj"
+  "CMakeFiles/main1.dir/CS112-203-2nd-S3-20210049-20210067-20211053-A3-Part1.cpp.obj.d"
+  "libmain1.dll.a"
+  "main1.exe"
+  "main1.exe.manifest"
+  "main1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
