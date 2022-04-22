fa5209e90e00181427223c9c58d839b03e51bf21
shahdmoamen <shahdmoamem@gmail.com>
2022-04-22 14:29:30 +0200

    edge detection update

---
+++ cmake-build-debug/CMakeFiles/target-1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/target-1.dir/CS112-203-2nd-S3-20210049-20210067-20211053-A3-Full.cpp.obj"
+  "CMakeFiles/target-1.dir/CS112-203-2nd-S3-20210049-20210067-20211053-A3-Full.cpp.obj.d"
+  "libtarget-1.dll.a"
+  "target-1.exe"
+  "target-1.exe.manifest"
+  "target-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/target-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
